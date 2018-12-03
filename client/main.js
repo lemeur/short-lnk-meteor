b86b5900de78be3ca41d50de9d7ffb44733aa546
Thibault Le Meur <lemeur@MacBook-Pro-de-Thibault.local>
2018-12-03 22:29:19 +0100

    Refactorisation du code de routage

--- client/main.js
+++ client/main.js
@@ -1,52 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDom from 'react-dom';
-import {Router, Route, browserHistory } from 'react-router';
 import {Tracker} from 'meteor/tracker';
+import {routes, onAuthChange} from './../imports/routes/routes';
 
 
-import Signup from './../imports/ui/Signup';
-import Link from './../imports/ui/Link';
-import NotFound from './../imports/ui/NotFound';
-import Login from './../imports/ui/Login';
-
-const onEnterPublicPage = () => {
-  if (Meteor.userId()) {
-    browserHistory.replace('/links');
-  }
-}
-const onEnterPrivatePage = () => {
-  if (!Meteor.userId()) {
-    browserHistory.replace('/');
-  }
-}
-const routes = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Login} onEnter={onEnterPublicPage}/>
-    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage}/>
-    <Route path="/links" component={Link} onEnter={onEnterPrivatePage}/>
-    <Route path="*" component={NotFound} onEnter={onEnterPrivatePage}/>
-  </Router>
-);
-
-//window.bh = browserHistory;
-
-
-const unAuthenticatedPages = ['/','/signup'];
-const authenticatedPages = ['/links'];
 
 Tracker.autorun( () => {
   const isAuthenticated = !!Meteor.userId(); // True if not null or empty
-  const pathName = browserHistory.getCurrentLocation().pathname;
-  console.log('isAuthentcated:',isAuthenticated, ' on ',pathName);
-  const isUnauthenticatedPage = unAuthenticatedPages.includes(pathName);
-  const isAthenticatedPage = authenticatedPages.includes(pathName);
-  if (isUnauthenticatedPage && isAuthenticated) {
-    browserHistory.replace('/links');
-  }
-  else if (authenticatedPages && !isAuthenticated) {
-    browserHistory.replace('/');
-  }
+  onAuthChange(isAuthenticated);
 });
 
 Meteor.startup(() => {
--- server/main.js
+++ server/main.js
@@ -1,22 +1,8 @@
 import { Meteor } from 'meteor/meteor';
-import SimpleSchema from 'simpl-schema';
-import {Accounts} from 'meteor/accounts-base';
+import '../imports/api/users'; // only import and execute
 
 Meteor.startup(() => {
 
-  Accounts.validateNewUser((user) => {
-    const email = user.emails[0].address;
-    try {
-      new SimpleSchema({
-        email: {
-          type: String,
-          regEx: SimpleSchema.RegEx.Email
-        }
-      }).validate({ email });
-    } catch (e) {
-      throw new Meteor.Error(400, e.message); // Will be send on the client side
-    }
-    return true;
-  });
+
 
 });
