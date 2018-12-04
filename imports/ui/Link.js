576ca9536d55980a1d009408511f29a882928407
Thibault Le Meur <lemeur@MacBook-Pro-de-Thibault.local>
2018-12-04 22:33:52 +0100

    Prepare the Links collection and Add form

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
 import  {Accounts}  from 'meteor/accounts-base';
+import {Links} from '../api/links';
 
 export default class Link extends React.Component {
   onLogout() {
     console.log('logout');
     Accounts.logout();
     //browserHistory.push('/');
-    
+
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    const url = this.refs.url.value.trim();
+    if (url) {
+      Links.insert({
+        url
+      });
+      this.refs.url.value = "";
+    }
+
   }
   render() {
     return (
         <div>
           <p>Your links</p>
           <button onClick={this.onLogout.bind(this)}>Logout</button>
+          <p>Add Link</p>
+          <form onSubmit={this.onSubmit.bind(this)}>
+            <input type="text" ref="url" placeholder="url"/>
+            <button>Add Link</button>
+          </form>
         </div>
     );
   }
