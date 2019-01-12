1721facdfa0a544c245272e7746a82e2aa9a3dae
Thibault Le Meur <lemeur@MacBook-Pro-de-Thibault.local>
2019-01-13 00:03:35 +0100

    Controlled Inputs for AddLink and LinksListFilters

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -3,22 +3,43 @@ import {Meteor} from 'meteor/meteor';
 
 
 export default class AddLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: ''
+    }
+  }
   onSubmit(e) {
+    //const url = this.state.url;
+    // equivalent to:
+    const {url} = this.state;
     e.preventDefault();
-    const url = this.refs.url.value.trim();
     if (url) {
-      Meteor.call('links.insert', url);
-      this.refs.url.value = "";
+      Meteor.call('links.insert', url, (err, res) => {
+      if (!err) {
+        // If there is NO error, reset the URL in state
+        this.setState({url: ''})
+      }
+      });
     }
 
-  }
 
+  }
+  onChange(e) {
+    this.setState({
+        url: e.target.value
+    });
+  }
   render() {
     return (
       <div>
         <h2>Add Link</h2>
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="text" ref="url" placeholder="url"/>
+          <input type="text"
+                 placeholder="url"
+                 value={this.state.url}
+                 onChange={this.onChange.bind(this)}
+          />
           <button>Add Link</button>
         </form>
       </div>
