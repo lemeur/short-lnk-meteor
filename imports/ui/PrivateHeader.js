34728f00fe0942040081ab2a7b8ffb4ea0b05432
Thibault Le Meur <lemeur@MacBook-Pro-de-Thibault.local>
2019-01-19 18:32:06 +0100

    Styling Header

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -8,10 +8,11 @@ const PrivateHeader = (props) => {
         Accounts.logout();
       }
       return (
-        <div>
-          <h1>{props.title}</h1>
-          <button onClick={onLogout}>Logout</button>
-          {/*  <button onClick={() => Accounts.logout() }>Logout</button>  */}
+        <div className="header">
+          <div className="header__content">
+            <h1 className="header__title">{props.title}</h1>
+            <button className="button button--link-text" onClick={onLogout}>Logout</button>
+          </div>
         </div>);
 }
 
